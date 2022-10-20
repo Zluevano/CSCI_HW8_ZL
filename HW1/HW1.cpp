a162bd180a43d11a9a91f1ffbc4cf7de9d3c0ea5
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-10-20 21:25:53 +0000

    When I said HW1 I realized I meant HW4 but HW1, anyhow HW1 is nearing completion just have to figure out weighted grades...

--- HW1/HW1.cpp
+++ HW1/HW1.cpp
@@ -5,15 +5,24 @@ using namespace std;
 
 // Prototypes
 void printMatrix(int myMat[][7], int n_rows, int n_colum);
+void gradeinput(int myMat[][7], int n_row, int n_colunm);
 
 int main()
 {
+//Declare Variables
 int students;
+int add = 1;
+int sum = 0;
+int add2 = 0;
+
 cout << "Input the number of students:" << endl;
 cin >> students;
-int grades[students][7];
-int add = 1;
 
+//Declare 2D Array
+int grades[students][7];
+
+
+//Set the first column to 1 - students.
 for (int r = 0; r < students; r++)
   {
   for (int c = 0; c <1; c++)
@@ -23,9 +32,45 @@ for (int r = 0; r < students; r++)
       }
   }
 
+//Test print to verify the prior.
 printMatrix(grades, students, 7);
-}
 
+
+//This starts at column 2 as column 1 was used for the student.
+cout << "input the 4 grades." << endl;
+for (int r = 0; r < students; r++)
+  {
+   for (int c = 1; c < 5; c++)
+     {
+        cin >> grades[r][c];
+      // sum = grades[r][c] + sum;
+      // grades[r][5] = sum;
+     }
+  }
+
+//cout << sum;
+cout << endl;
+printMatrix(grades, students, 7);
+
+
+//Try and find the average.
+cout << "The average:" << endl;
+for (int r = 0; r < students; r++)
+  {
+   for (int c = 1; c < 5; c++)
+     {
+       sum = grades[r][c] + sum;
+       grades[r][5] = sum/4;
+     }
+    sum = 0;
+  }
+
+cout << endl;
+printMatrix(grades, students, 7);
+
+  
+  
+}
 
 
 void printMatrix(int myMat[][7], int n_rows, int n_colum)
@@ -38,4 +83,5 @@ void printMatrix(int myMat[][7], int n_rows, int n_colum)
     }
   cout << endl;
   }
+    
 }
