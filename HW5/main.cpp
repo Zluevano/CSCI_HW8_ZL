99e668307532f4689e85c19a4643e91a7866501d
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-10-27 05:48:22 +0000

    HW5 Finished Added Comments and removed unused variables.

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -1,3 +1,11 @@
+/*
+Programmer: Zachary Luevano
+Description: Takes a 2 x 2 matrix finds the determinent and then proceedes to find the inverse using the determiment and flipping some components on the matrix, unsure 
+if I've seen this before but programming matrixes for the purpose of math dramatically simplifiers process compared to the tradational way of doing things row by row.
+Math speaking the code itself labels what is done and makes the process incredibly easy to understand.
+Date Finished: 10/26/2022
+*/
+  
 #include <cmath>
 #include <iomanip>
 #include <iostream>
@@ -5,14 +13,14 @@
 using namespace std;
 
 // Prototype Functions
-void prdoubleMatrix(double myMat[][2], double n_rows, double n_colum);
-void findadj(double mymat[0][2], double n_rows, double n_colum);
+void printdoubleMatrix(double myMat[][2], double n_rows, double n_colum);
+void findAdj(double mymat[0][2], double n_rows, double n_colum);
 void findInv(double mymat[0][2], double n_rows, double n_colum, double det);
 
 int main() {
+// Declare Variable and Array
   double det;
   double mat[2][2];
-  double inv[2][2];
 
   cout << "Input a 2 x 2 matrix by row." << endl;
   for (int r = 0; r < 2; r++) {
@@ -20,7 +28,10 @@ int main() {
       cin >> mat[r][c];
     }
   }
-  prdoubleMatrix(mat, 2, 2);
+
+  cout << endl;
+  cout << "Now Displaying Matrix:" << endl;
+  printdoubleMatrix(mat, 2, 2);
   cout << endl;
 
 
@@ -39,7 +50,7 @@ return 0;
   
 cout << endl;
 cout << "Changing principle and flipping signs on other diagonal.";
-  
+cout << endl;
 //Change the signs of the others and account for a -0 as appearntly that's a thing with doubles.
 if (mat[0][1] == 0)
   mat[0][1] = mat[0][1];
@@ -51,39 +62,37 @@ if (mat[1][0] == 0)
 else
   mat[1][0] = -mat[1][0];
 
-
-double adj[2][2] = {mat[1][1], mat[0][1], 
-                 mat[1][0], mat[0][0]};
   
 cout << endl;
-cout << "Now Verifying new matrix." << endl;
-findadj(mat, 2, 2);
+cout << "Now Verifying new matrix:" << endl;
+findAdj(mat, 2, 2);
 cout << endl;
 
 
+cout << "Now Displaying Inverse Matrix:" << endl;
 findInv(mat, 2, 2, det);
   
   return 0;
 }
 
 // Prdouble matrix function does what you think it does.
-void prdoubleMatrix(double myMat[][2], double n_rows, double n_colum) {
+void printdoubleMatrix(double myMat[][2], double n_rows, double n_colum) {
   for (int r = 0; r < n_rows; r++) {
     for (int c = 0; c < n_colum; c++) {
-      cout << setw(5) << myMat[r][c];
+      cout << setw(10) << myMat[r][c];
     }
     cout << endl;
   }
 }
 
 // Find the adj of the matrix.
-void findadj(double mymat[0][2], double n_rows, double n_colum)
+void findAdj(double mymat[0][2], double n_rows, double n_colum)
 {
 double adj[2][2] = {mymat[1][1], mymat[0][1], 
                  mymat[1][0], mymat[0][0]};
 for (int r = 0; r < n_rows; r++) {
     for (int c = 0; c < n_colum; c++) {
-      cout << setw(5) << adj[r][c];
+      cout << setw(10) << adj[r][c];
     }
     cout << endl;
   }
@@ -97,7 +106,7 @@ double inv[2][2] = {mymat[1][1]/det, mymat[0][1]/det,
                  mymat[1][0]/det, mymat[0][0]/det};
 for (int r = 0; r < n_rows; r++) {
     for (int c = 0; c < n_colum; c++) {
-      cout << setw(5) << inv[r][c];
+      cout << setw(10) << inv[r][c];
     }
     cout << endl;
   }
