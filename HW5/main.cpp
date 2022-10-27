6dc52911086c39afe03e2aaed2cf7b35424061a9
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-10-27 05:21:22 +0000

    Realized when you took the inverse you had to divide by a number so I switched everything to a double, HW5 should be done, just need to do test cases.

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
-void printMatrix(int myMat[][2], int n_rows, int n_colum);
-void findadj(int mymat[0][2], int n_rows, int n_colum);
+// Prototype Functions
+void prdoubleMatrix(double myMat[][2], double n_rows, double n_colum);
+void findadj(double mymat[0][2], double n_rows, double n_colum);
+void findInv(double mymat[0][2], double n_rows, double n_colum, double det);
 
 int main() {
-  int det;
-  int mat[2][2];
-  int inv[2][2];
+  double det;
+  double mat[2][2];
+  double inv[2][2];
 
   cout << "Input a 2 x 2 matrix by row." << endl;
   for (int r = 0; r < 2; r++) {
@@ -18,28 +20,39 @@ int main() {
       cin >> mat[r][c];
     }
   }
-  printMatrix(mat, 2, 2);
+  prdoubleMatrix(mat, 2, 2);
   cout << endl;
 
 
 //Save the Det for later in the inverse matrix.
 det = (((mat[0][0]) * (mat[1][1])) - ((mat[0][1]) * (mat[1][0])));
-  
+
+
+//Message Display for deteriment and case for if it's 0 as  you can't divide by 0 aswell as terminate program.
 cout << "Determinent = ";
 if (((mat[0][0]) * (mat[1][1])) - ((mat[0][1]) * (mat[1][0])) != 0)
 cout << ((mat[0][0]) * (mat[1][1])) - ((mat[0][1]) * (mat[1][0])) << endl;
-else
-cout << "Cannot take inverse with 0 being determinent" << endl;
-
-
+else{
+cout << "Cannot take inverse with 0 being determinent as inverse will divide adj by 0." << endl;
+return 0;
+}
+  
 cout << endl;
 cout << "Changing principle and flipping signs on other diagonal.";
   
-//Change the signs of the others.
-mat[0][1] = -mat[0][1];
-mat[1][0] = -mat[1][0];
+//Change the signs of the others and account for a -0 as appearntly that's a thing with doubles.
+if (mat[0][1] == 0)
+  mat[0][1] = mat[0][1];
+else
+  mat[0][1] = -mat[0][1];
+  
+if (mat[1][0] == 0)
+  mat[1][0] = mat[1][0];
+else
+  mat[1][0] = -mat[1][0];
 
-int adj[2][2] = {mat[1][1], mat[0][1], 
+
+double adj[2][2] = {mat[1][1], mat[0][1], 
                  mat[1][0], mat[0][0]};
   
 cout << endl;
@@ -47,21 +60,14 @@ cout << "Now Verifying new matrix." << endl;
 findadj(mat, 2, 2);
 cout << endl;
 
-cout << "Displaying the inverse matrix now." << endl;
-for (int r = 0; r < 2; r++)
-  {
-    for (int c = 0; c < 2; c++)
-      {
-        inv[r][c] = findadj(mat, 2, 2)
-      }
-  }
 
-
+findInv(mat, 2, 2, det);
+  
   return 0;
 }
 
-// Print matrix function does what you think it does.
-void printMatrix(int myMat[][2], int n_rows, int n_colum) {
+// Prdouble matrix function does what you think it does.
+void prdoubleMatrix(double myMat[][2], double n_rows, double n_colum) {
   for (int r = 0; r < n_rows; r++) {
     for (int c = 0; c < n_colum; c++) {
       cout << setw(5) << myMat[r][c];
@@ -71,9 +77,9 @@ void printMatrix(int myMat[][2], int n_rows, int n_colum) {
 }
 
 // Find the adj of the matrix.
-void findadj(int mymat[0][2], int n_rows, int n_colum)
+void findadj(double mymat[0][2], double n_rows, double n_colum)
 {
-int adj[2][2] = {mymat[1][1], mymat[0][1], 
+double adj[2][2] = {mymat[1][1], mymat[0][1], 
                  mymat[1][0], mymat[0][0]};
 for (int r = 0; r < n_rows; r++) {
     for (int c = 0; c < n_colum; c++) {
@@ -85,13 +91,13 @@ return;
 }
 
 //find the inverse of the matrix;
-void findInv(int mymat[0][2], int n_rows, int n_colum, int det;
+void findInv(double mymat[0][2], double n_rows, double n_colum, double det)
 {
-int adj[2][2] = {mymat[1][1]/det, mymat[0][1]/det, 
+double inv[2][2] = {mymat[1][1]/det, mymat[0][1]/det, 
                  mymat[1][0]/det, mymat[0][0]/det};
 for (int r = 0; r < n_rows; r++) {
     for (int c = 0; c < n_colum; c++) {
-      cout << setw(5) << adj[r][c];
+      cout << setw(5) << inv[r][c];
     }
     cout << endl;
   }
